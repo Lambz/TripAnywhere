ca2ce3b6793819de71a46a4641f1df0d02dfb536
Chaitanya Sanoriya <chaitanya.sanoriya@gmail.com>
2020-05-30 01:36:18 -0400

    Flight Search + Search Result Completed

--- scripts/basicFunctions.js
+++ scripts/basicFunctions.js
@@ -1,6 +1,12 @@
 function capitalizeFirstLetter(string) 
 {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+	var str = "";
+	var st = string.split(" ");
+	st.forEach(element => {
+        str += element.charAt(0).toUpperCase()+element.slice(1)+" ";
+    });
+    console.log(str);
+	return str;
 }
 
 function getDateMonth(string)
@@ -19,5 +25,45 @@ function getDateMonth(string)
     month[9] = "Oct";
     month[10] = "Nov";
     month[11] = "Dec";
-    return d.getDay()+" "+month[d.getMonth()];
+    return d.getDate()+" "+month[d.getMonth()];
+}
+
+function getCarrierName(id, response)
+{
+    var carrier;
+    response.Carriers.forEach(element => {
+        //console.log("id: "+id+" element.CarrierId: "+element.CarrierId);
+        if(id == element.CarrierId)
+        {
+            //console.log("returning: "+element.Name);
+            carrier = element.Name;
+        }
+    });
+    return carrier;
+}
+
+function getNotCarrierName(id, response)
+{
+    var carrier;
+    response.Carriers.forEach(element => {
+        //console.log("id: "+id+" element.CarrierId: "+element.CarrierId);
+        if(id != element.CarrierId)
+        {
+            //console.log("returning: "+element.Name);
+            carrier = element.Name;
+        }
+    });
+    return carrier;
+}
+
+function getPlace(id, response)
+{
+    var place;
+    response.Places.forEach(element => {
+        if(id == element.PlaceId)
+        {
+            place = element.CityName;
+        }
+    });
+    return place;
 }
