ed7823dae4c2b556bc35d453ff5483dcc189088b
Chetan Singh <chetan.singh.0602@gmail.com>
2020-06-04 20:30:18 -0400

    fixed search in index.html | updated scripts

--- trains/scripts/loadData.js
+++ trains/scripts/loadData.js
@@ -1,43 +1,63 @@
-let db = null;
-// checks if any user logged in
-if(sessionStorage.getItem("activeUser")) {
-  openDb();
+
+// Static dummy data for the search results
+var routes = [{trainName: "Via Rail 003", src: "Toronto", dest: "Ottawa", duration: "3 hours"},
+{trainName: "Via Rail 021", src: "Montreal" , dest: "Calgary", duration: "21 hours"},
+{trainName: "Via Rail 112", src: "Vancouver", dest: "Waterloo", duration: "36 hours"},
+{trainName: "Via Rail 091", src: "Edmonton", dest: "Oshawa", duration: "12 hours"},
+{trainName: "Via Rail 023", src: "Hamilton", dest: "Montreal", duration: "15 hours"}
+];
+
+// method to be invoked on form submission
+// stores seach results
+let index = [];
+searchTrains();
+// invokes the data search on basis of the user input
+// for desktop only
+
+function getDesktopQuery() {
+  // saves data to session storage
+  src = document.getElementById('desktop_source').value;
+  sessionStorage.setItem("source", src);
+  dest = document.getElementById('desktop_destination').value;
+  sessionStorage.setItem("destination", dest);
+  date = document.getElementById('desktop_date').value;
+  sessionStorage.setItem("date", date);
+  window.location.href = "./search-result.html";
 }
-// opens db to check user bookings
-function openDb() {
-  const request = indexedDB.open("Trip_Anywhere", 1);
 
-  request.onsuccess = e => {
-    db = e.target.result;
-    showData();
-  }
-
-  request.onerror = e => {
-    alert(`Could not fetch records! Error: ${e.error.message}`);
-  }
-}
-// shows data by checking the previous bookings if any
-function showData() {
-  const tx = db.transaction("trains");
-  const dataChk = tx.objectStore("trains");
-  let data = dataChk.openCursor();
-
-  data.onsuccess = e => {
-    // div where the data will be showed
-    let userData = document.getElementsByClassName('user_data');
-    let cursor = e.target.result;
-    let msg = "";
-    if(cursor) {
-      if(cursor.value.email === sessionStorage.getItem("activeUser")) {
-        // adds the item to msg
-        msg = msg + cursor.value + "<br />";
-        cursor.continue();
-      }
-      else {
-        cursor.continue();
-      }
+// searches the train data
+function searchTrains() {
+  // clears previous search results
+  index = [];
+  routes.forEach(function(node) {
+    let x = node.src;
+    let y = node.dest;
+    let m = sessionStorage.getItem("source");
+    let n = sessionStorage.getItem("destination");
+    if(x.toUpperCase === m.toUpperCase && y.toUpperCase === n.toUpperCase) {
+      index.push(node);
     }
-    // shows data if any
-    userData.innerHTML = msg;
+  });
+  showResult();
+}
+// shows the result if any
+function showResult() {
+  let header = document.getElementById('search_header');
+  let result = document.getElementById("search_result");
+  if(index) {
+    header.innerHTML = `The results for trains between ${sessionStorage.getItem("source")} and ${sessionStorage.getItem("destination")}`;
+    let msg = `<div id="result_cards">`;
+    index.forEach(function(node) {
+      // adds card to the html code
+      let card = `<div class="card"><p class="card_head">Train# ${node.trainName}</p><br /><p class="card_body">Source: ${node.src}<br />
+      Destination: ${node.dest}</p><p class="card_dest">Journey duration: ${node.duration}</p></div>`;
+      msg += card;  
+    }); 
+    msg += "</div>";
+    // sets the cards to div
+    result.innerHTML = msg;
+  }
+  else {
+    header.innerHTML = "Sorry! No trains found on this route."
   }
 }
