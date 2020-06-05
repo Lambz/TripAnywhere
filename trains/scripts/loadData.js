dafac2224794927d65b3f467a24b389a618dbaaf
Chetan Singh <chetan.singh.0602@gmail.com>
2020-06-04 21:36:14 -0400

    finalized card style for result| fixed search results

--- trains/scripts/loadData.js
+++ trains/scripts/loadData.js
@@ -1,10 +1,10 @@
 
 // Static dummy data for the search results
-var routes = [{trainName: "Via Rail 003", src: "Toronto", dest: "Ottawa", duration: "3 hours"},
-{trainName: "Via Rail 021", src: "Montreal" , dest: "Calgary", duration: "21 hours"},
-{trainName: "Via Rail 112", src: "Vancouver", dest: "Waterloo", duration: "36 hours"},
-{trainName: "Via Rail 091", src: "Edmonton", dest: "Oshawa", duration: "12 hours"},
-{trainName: "Via Rail 023", src: "Hamilton", dest: "Montreal", duration: "15 hours"}
+var routes = [{trainName: "Via Rail 003", src: "Toronto", dest: "Ottawa", duration: "3 hours", id: "1"},
+{trainName: "Via Rail 021", src: "Montreal" , dest: "Calgary", duration: "21 hours", id: "2"},
+{trainName: "Via Rail 112", src: "Vancouver", dest: "Waterloo", duration: "36 hours", id: "3"},
+{trainName: "Via Rail 091", src: "Edmonton", dest: "Oshawa", duration: "12 hours", id: "4"},
+{trainName: "Via Rail 023", src: "Hamilton", dest: "Montreal", duration: "15 hours", id: "5"}
 ];
 
 // method to be invoked on form submission
@@ -30,11 +30,8 @@ function searchTrains() {
   // clears previous search results
   index = [];
   routes.forEach(function(node) {
-    let x = node.src;
-    let y = node.dest;
-    let m = sessionStorage.getItem("source");
-    let n = sessionStorage.getItem("destination");
-    if(x.toUpperCase === m.toUpperCase && y.toUpperCase === n.toUpperCase) {
+    if(node.src.toUpperCase() === sessionStorage.getItem("source").toUpperCase() &&
+     node.dest.toUpperCase() === sessionStorage.getItem("destination").toUpperCase()) {
       index.push(node);
     }
   });
@@ -45,12 +42,13 @@ function showResult() {
   let header = document.getElementById('search_header');
   let result = document.getElementById("search_result");
   if(index) {
-    header.innerHTML = `The results for trains between ${sessionStorage.getItem("source")} and ${sessionStorage.getItem("destination")}`;
+    header.innerHTML = `<p id="gen_header">The result for trains between ${sessionStorage.getItem("source").toUpperCase()}
+     and ${sessionStorage.getItem("destination").toUpperCase()}</p>`;
     let msg = `<div id="result_cards">`;
     index.forEach(function(node) {
       // adds card to the html code
-      let card = `<div class="card"><p class="card_head">Train# ${node.trainName}</p><br /><p class="card_body">Source: ${node.src}<br />
-      Destination: ${node.dest}</p><p class="card_dest">Journey duration: ${node.duration}</p></div>`;
+      let card = `<div class="card"><p class="card_head">Train Name: ${node.trainName}</p><br /><p class="card_body"><b>Source:</b> ${node.src}<br />
+      <b>Destination:</b> ${node.dest}</p><p class="card_dest"><b>Journey duration:</b> ${node.duration}</p><button id=${node.id} onclick="getBooking()">Book Now</button></div>`;
       msg += card;  
     }); 
     msg += "</div>";
@@ -58,6 +56,6 @@ function showResult() {
     result.innerHTML = msg;
   }
   else {
-    header.innerHTML = "Sorry! No trains found on this route."
+    header.innerHTML = `<p id="gen_header">Sorry! No trains found on this route.</p>`;
   }
 }
