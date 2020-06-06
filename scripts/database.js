5c76b9e0cba142b779ecf455058f56694bd876b8
Chaitanya Sanoriya <chaitanya.sanoriya@gmail.com>
2020-06-05 22:24:52 -0400

    Sign Up Complete

--- scripts/database.js
+++ scripts/database.js
@@ -68,85 +68,11 @@ function createDb() {
 		}
 		
 	}
-
-
-
-	// window.indexedDB = window.indexedDB || window.mozIndexedBD || window.webkitIndexedDB || window.msIndexedDB;
-	// let request = window.indexedDB.open("Trip_Anywhere", 1), db, tx, store, index;
-
-	// request.onsuccess = function (e) 
-	// {
-	// 	db = request.result;
-	// 	tx = db.transaction("user", "readwrite");
-	// 	store = tx.objectStore("user");
-	// 	db.onerror = function (e) {
-	// 		console.log("ERROR " + e.target.errorCode);
-	// 	}
-
-	// 	let msgDiv = document.getElementById('msgDiv');
-	// 	// defines a node for entry to db
-	// 	let user = {
-	// 		email: useremail,
-	// 		fname: firstName,
-	// 		lname: lastName,
-	// 		gdr: gender,
-	// 		pwd: password
-	// 	};
-	// 	// creates transaction
-	// 	const tx = db.transaction("user", "readwrite");
-	// 	tx.onerror = e => 
-	// 	{
-	// 		document.getElementById("user_already_exists").style.display = "block";
-	// 	}
-
-	// 	const userAdd = tx.objectStore("user");
-	// 	// adds user 
-	// 	userAdd.add(user);
-	// 	// adds user as active user to track
-	// 	sessionStorage.setItem("activeUser", user.email);
-	// 	sessionStorage.setItem("fname",user.fname);
-
-	// 	// addUser(db);
-	// 	// afterRegister();
-	// }
-
-	// // on error
-
-	// request.onerror = e => {
-	// 	alert(`Error: Couldn't register user. ErrorName: ${e.target.error.message}`);
-	// }
 }
 
-// // adds user to the db
-// function addUser(db) {
-// 	// gets form data
-
-// 	// to show the message to user
-// 	let msgDiv = document.getElementById('msgDiv');
-// 	// defines a node for entry to db
-// 	let user = {
-// 		email: useremail,
-// 		fname: firstName,
-// 		lname: lastName,
-// 		gdr: gender,
-// 		pwd: password
-// 	};
-// 	// creates transaction
-// 	const tx = db.transaction("user", "readwrite");
-// 	tx.onerror = e => {
-// 		msgDiv.innerHTML = "User already exists!";
-// 	}
-
-// 	const userAdd = tx.objectStore("user");
-// 	// adds user 
-// 	userAdd.add(user);
-// 	// adds user as active user to track
-// 	sessionStorage.setItem("activeUser", user.email);
-// }
 // clears screen and shows homepage button and message
 function afterRegister() {
-	// let page = `<h2>User Registered Successfully</h2><br /><a href="./index.html">Back to homepage</a>`;
-	// document.querySelector('body').innerHTML = page;
+
 	document.getElementById("signup_successful").style.display = "block";
 	window.setTimeout(function () {
 		window.location.href = "index.html";
